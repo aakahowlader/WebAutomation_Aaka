4b210f956d79281f17749f821ae9562d0b4ec303
aakahowlader <howlader1995@yahoo.com>
2021-03-06 21:30:38 -0500

    idk

--- src/Amazon/HomePage.java
+++ src/Amazon/HomePage.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Shihab\\chromedriver.exe");
         WebDriver obj = new ChromeDriver();
         obj.get("https://Amazon.com");
+        Thread.sleep(5000);
+
+        obj.close();
 
 
 
