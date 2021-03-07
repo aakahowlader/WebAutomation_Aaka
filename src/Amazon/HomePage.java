0304070a0b7313f4606602cdea407fa3857ee7f1
aakahowlader <howlader1995@yahoo.com>
2021-03-06 21:21:50 -0500

    whatever

--- src/Amazon/HomePage.java
+++ src/Amazon/HomePage.java
@@ -1,7 +1,22 @@
 package Amazon;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
 
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Shihab\\chromedriver.exe");
+        WebDriver obj = new ChromeDriver();
+        obj.get("https://Amazon.com");
+
+
+
+
+
+    }
 
 
 }
